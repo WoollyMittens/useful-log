1c86fd829aa5ced0b5cd11968c0427a0e7b24f90
Maurice van Creij <maurice.vancreij@webqem.com>
2019-02-22 07:52:32 +1100

    Update require support.

--- src/js/log.js
+++ src/js/log.js
@@ -6,52 +6,40 @@
 	This work is licensed under a Creative Commons Attribution 3.0 Unported License.
 */
 
-(function(){
-
-	// PRIVATE
-
-	var popup, messages = [];
-
-	// PUBLIC
-
-	var log = function () {
-		var a, b, message;
-		// for all arguments
-		for (a = 0, b = arguments.length; a < b; a += 1) {
-			// add the log entry to the top
-			message = (arguments[a] !== null && arguments[a] !== undefined) ? JSON.stringify(arguments[a]) : arguments[a];
-			messages.push(message);
-		}
-		// if the log window exists
-		if (popup && /div/i.test(popup.nodeName)) {
-			// write to the log
-			popup.innerHTML = messages.reverse().join('<br/>');
-		}
-		// else create one
-		else {
-			popup = document.createElement('div');
-			popup.style.position = 'fixed';
-			popup.style.right = '0px';
-			popup.style.bottom = '0px';
-			popup.style.width = '320px';
-			popup.style.height = '240px';
-			popup.style.color = 'Black';
-			popup.style.backgroundColor = 'Yellow';
-			popup.style.opacity = 0.7;
-			popup.style.padding = '1em';
-			popup.style.zIndex = 100000;
-			popup.style.overflow = 'auto';
-			popup.innerHTML = messages.reverse().join('<br/>');
-			document.body.appendChild(popup);
-		}
-	};
-
-	// make public
-	window.log = log;
-
-	// return as a require.js module
-	if (typeof module !== 'undefined') {
-		exports = module.exports = log;
+var log = function () {
+	var a, b, message;
+	window.log_popup = window.log_popup || null;
+	window.log_messages = window.log_messages || [];
+	// for all arguments
+	for (a = 0, b = arguments.length; a < b; a += 1) {
+		// add the log entry to the top
+		message = (arguments[a] !== null && arguments[a] !== undefined) ? JSON.stringify(arguments[a]) : arguments[a];
+		log_messages.push(message);
 	}
+	// if the log window exists
+	if (log_popup && /div/i.test(log_popup.nodeName)) {
+		// write to the log
+		log_popup.innerHTML = log_messages.reverse().join('<br/>');
+	}
+	// else create one
+	else {
+		log_popup = document.createElement('div');
+		log_popup.style.position = 'fixed';
+		log_popup.style.right = '0px';
+		log_popup.style.bottom = '0px';
+		log_popup.style.width = '320px';
+		log_popup.style.height = '240px';
+		log_popup.style.color = 'Black';
+		log_popup.style.backgroundColor = 'Yellow';
+		log_popup.style.opacity = 0.7;
+		log_popup.style.padding = '1em';
+		log_popup.style.zIndex = 100000;
+		log_popup.style.overflow = 'auto';
+		log_popup.innerHTML = log_messages.reverse().join('<br/>');
+		document.body.appendChild(log_popup);
+	}
+};
 
-})();
+// return as a require.js module
+if (typeof define != 'undefined') define(['log'], function () { return log });
+if (typeof module != 'undefined') module.exports = log;
